c1b7e30eb37ff2666dc5e493e120f3549ef799ea
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2016-03-17 10:54:24 +0100

    infra code

---
+++ infrastructure/prod.js
@@ -0,0 +1,29 @@
+var heroin = require('heroin-js');
+
+var configurator = heroin(process.env.HEROKU_API_TOKEN, {debug: false});
+
+var prod = {
+    name: 'book-inventory-prod',
+    region: 'eu',
+    maintenance: false,
+    stack: 'cedar-14',
+    config_vars: {
+        MONGOLAB_URI: process.env.MONGOLAB_URI,
+        NAME: 'mateusz'
+    },
+    addons: {mongolab: {plan: 'mongolab:sandbox'}},
+    collaborators: ['kwasniewski.mateusz@gmail.com'],
+    features: {
+        'runtime-dyno-metadata': {enabled: false},
+        'log-runtime-metrics': {enabled: false},
+        'http-session-affinity': {enabled: false},
+        preboot: {enabled: false},
+        'http-shard-header': {enabled: false},
+        'http-end-to-end-continue': {enabled: false}
+    },
+    formation: [{process: 'web', quantity: 1, size: 'Free'}],
+    log_drains: [],
+    domains: ['book-inventory-prod.herokuapp.com']
+};
+
+configurator(prod);
